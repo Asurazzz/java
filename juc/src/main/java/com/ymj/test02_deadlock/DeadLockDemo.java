079a8374f87fb54b7b9e351c7a3871810d747ac5
ymj <2513667783@qq.com>
2021-05-31 16:38:45 +0800

    死锁demo

---
+++ juc/src/main/java/com/ymj/test02_deadlock/DeadLockDemo.java
@@ -0,0 +1,41 @@
+package com.ymj.test02_deadlock;
+
+
+/**
+ * @Classname DeadLockDemo
+ * @Description TODO
+ * @Date 2021/5/31 15:51
+ * @Created by yemingjie
+ */
+public class DeadLockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        new Thread(() -> {
+            synchronized (A) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (B) {
+                    System.out.println("1");
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (B) {
+                synchronized (A) {
+                    System.out.println("2");
+                }
+            }
+        }).start();
+    }
+
+}
