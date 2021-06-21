8b9aa60dd77648d25cbb035a4eeb476cdc5e1c66
ymj <2513667783@qq.com>
2021-06-21 10:19:15 +0800

    添加optional注释

--- base/src/com/ymj/code07_optional/OptionalDemo.java
+++ base/src/com/ymj/code07_optional/OptionalDemo.java
@@ -4,7 +4,23 @@ import java.util.Optional;
 
 /**
  * @Classname OptionalDemo
- * @Description TODO
+ * @Description
+ *
+ * Optional.of：创建一个Optional实例
+ *
+ * Optional.empty：创建一个空的Optional实例
+ *
+ * Optional.ofNullable：若t不为null，创建optional实例，否者创建一个空实例
+ *
+ * isPresent：判断是否包含值
+ *
+ * orElse(T t)：如果对象包含值，则返回该值，否则返回t
+ *
+ * orElseGet(Supplier s)：如果调用对象包含值，返回该值，否则返回S获取的值
+ *
+ * map(Function f)：如果有值对其处理，返回处理后的Optional，否则返回Optional.empty()
+ *
+ * flatMap(Function mapper)：与map类似，要求返回值必须是Optional
  * @Date 2021/6/21 10:08
  * @Created by yemingjie
  */
