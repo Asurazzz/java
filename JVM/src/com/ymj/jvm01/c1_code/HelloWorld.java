33c91469ea0a7f846161b198c56c75cba4803f40
ymj <2513667783@qq.com>
2021-08-09 11:14:11 +0800

    JVM的HelloWorld

---
+++ JVM/src/com/ymj/jvm01/c1_code/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.ymj.jvm01.c1_code;
+
+/**
+ * @Classname HelloWorld
+ * @Description TODO
+ * @Date 2021/8/9 11:03
+ * @Created by yemingjie
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
