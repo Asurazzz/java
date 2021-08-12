31c554b147d40c465b95f43697c33391bd648d14
ymj <2513667783@qq.com>
2021-08-12 17:25:27 +0800

    方法区内存溢出

---
+++ JVM/src/com/ymj/jvm01/c3_code/JavaMethodAreaOOM.java
@@ -0,0 +1,25 @@
+package com.ymj.jvm01.c3_code;
+
+/**
+ * @Classname JavaMethodAreaOOM
+ * @Description TODO
+ * @Date 2021/8/12 17:20
+ * @Created by yemingjie
+ */
+public class JavaMethodAreaOOM {
+
+//    static class OOMObject { }
+//    public static void main(String[] args) {
+//        while (true) {
+//            Enhancer enhancer = new Enhancer();
+//            enhancer.setSuperclass(OOMObject.class);
+//            enhancer.setUseCache(false);
+//            enhancer.setCallback(new MethodInterceptor() {
+//                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws ThrowException{
+//                    return proxy.invokeSuper(obj, args);
+//                }
+//            });
+//            enhancer.create();
+//        }
+//    }
+}
