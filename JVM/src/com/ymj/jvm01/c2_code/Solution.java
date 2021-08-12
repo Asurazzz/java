15f61c0fd2ec9561d1840fe2fc74ab13d03eb4ef
ymj <2513667783@qq.com>
2021-08-12 11:50:46 +0800

    Solution

---
+++ JVM/src/com/ymj/jvm01/c2_code/Solution.java
@@ -0,0 +1,13 @@
+package com.ymj.jvm01.c2_code;
+
+/**
+ * @Classname Solution
+ * @Description TODO
+ * @Date 2021/8/12 11:50
+ * @Created by yemingjie
+ */
+public class Solution {
+    public void method() {
+        System.out.println("are you ok");
+    }
+}
