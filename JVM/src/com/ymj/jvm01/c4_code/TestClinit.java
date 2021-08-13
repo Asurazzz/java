c8356385bb4d5ce253e1bd76810b780061cd7485
ymj <2513667783@qq.com>
2021-08-13 15:34:39 +0800

    TestClinit

---
+++ JVM/src/com/ymj/jvm01/c4_code/TestClinit.java
@@ -0,0 +1,15 @@
+package com.ymj.jvm01.c4_code;
+
+/**
+ * @Classname TestClinit
+ * @Description TODO
+ * @Date 2021/8/13 15:27
+ * @Created by yemingjie
+ */
+public class TestClinit {
+//    static {
+//        i = 0; // 给变量复制可以正常编译通过
+//        System.out.println(i); // 这句编译器会提示“非法向前引用”
+//    }
+//    static int i = 1;
+}
---
+++ JVM/src/com/ymj/jvm01/c4_code/TestClinit02.java
@@ -0,0 +1,25 @@
+package com.ymj.jvm01.c4_code;
+
+/**
+ * @Classname TestClinit02
+ * @Description TODO
+ * @Date 2021/8/13 15:31
+ * @Created by yemingjie
+ */
+public class TestClinit02 {
+    static class Parent {
+        public static int A = 1;
+
+        static {
+            A = 2;
+        }
+    }
+
+    static class Sub extends Parent {
+        public static int B = A;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Sub.B);
+    }
+}
