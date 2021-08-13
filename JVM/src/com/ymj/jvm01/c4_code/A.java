8b1299b1d058f958a221eb298dcf32059e321483
ymj <2513667783@qq.com>
2021-08-13 15:00:45 +0800

    测试jvm准备阶段

---
+++ JVM/src/com/ymj/jvm01/c4_code/A.java
@@ -0,0 +1,15 @@
+package com.ymj.jvm01.c4_code;
+
+/**
+ * @Classname A
+ * @Description TODO
+ * @Date 2021/8/13 14:50
+ * @Created by yemingjie
+ */
+public class A {
+    static int a;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+}
---
+++ JVM/src/com/ymj/jvm01/c4_code/B.java
@@ -0,0 +1,14 @@
+package com.ymj.jvm01.c4_code;
+
+/**
+ * @Classname B
+ * @Description TODO
+ * @Date 2021/8/13 14:51
+ * @Created by yemingjie
+ */
+public class B {
+//    public static void main(String[] args) {
+//        int a;
+//        System.out.println(a);
+//    }
+}
