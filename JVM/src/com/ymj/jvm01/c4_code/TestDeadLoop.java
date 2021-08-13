6c2c593ed4a81e04a26db85e61df4ad58dcb33fa
ymj <2513667783@qq.com>
2021-08-13 15:38:56 +0800

    TestDeadLoop

---
+++ JVM/src/com/ymj/jvm01/c4_code/TestDeadLoop.java
@@ -0,0 +1,35 @@
+package com.ymj.jvm01.c4_code;
+
+/**
+ * @Classname TestDeadLoop
+ * @Description TODO
+ * @Date 2021/8/13 15:34
+ * @Created by yemingjie
+ */
+public class TestDeadLoop {
+    static class DeadLoopClass {
+        static {
+            // 如果不加上这个if语句， 编译器将提示“Initializer does not complete normally”并拒绝编译
+            if (true) {
+                System.out.println(Thread.currentThread() + " init DeadLoopClass");
+                while (true) {
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Runnable script = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread() + " start");
+                DeadLoopClass dlc = new DeadLoopClass();
+                System.out.println(Thread.currentThread() + " run over");
+            }
+        };
+        Thread thread1 = new Thread(script);
+        Thread thread2 = new Thread(script);
+        thread1.start();
+        thread2.start();
+    }
+}
