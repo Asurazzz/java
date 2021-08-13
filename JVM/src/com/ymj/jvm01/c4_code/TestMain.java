9930b8e1b4a7fb343bd8132094c34b1c3fa9b94d
ymj <2513667783@qq.com>
2021-08-13 15:27:07 +0800

    TestMain

---
+++ JVM/src/com/ymj/jvm01/c4_code/TestMain.java
@@ -0,0 +1,22 @@
+package com.ymj.jvm01.c4_code;
+
+/**
+ * @Classname TestMain
+ * @Description TODO
+ * @Date 2021/8/13 15:08
+ * @Created by yemingjie
+ */
+public class TestMain {
+    private static int i;
+    private double d;
+
+    public static void print() {
+
+    }
+
+    private boolean trueOrFalse() {
+        return false;
+    }
+}
+
+
