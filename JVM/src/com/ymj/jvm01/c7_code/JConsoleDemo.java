19564dc53df2355a0b551ae9b6fb024879cb197d
ymj <2513667783@qq.com>
2021-08-18 16:50:39 +0800

    JConsoleDemo

---
+++ JVM/src/com/ymj/jvm01/c7_code/JConsoleDemo.java
@@ -0,0 +1,30 @@
+package com.ymj.jvm01.c7_code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Classname JConsoleDemo
+ * @Description TODO
+ * @Date 2021/8/18 16:44
+ * @Created by yemingjie
+ */
+public class JConsoleDemo {
+    static class OOMObject {
+        public byte[] placeholder = new byte[64 * 1024];
+    }
+
+    public static void fillHeap(int num) throws InterruptedException {
+        List<OOMObject> list = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            Thread.sleep(50);
+            list.add(new OOMObject());
+        }
+        System.gc();
+    }
+
+    public static void main(String[] args) throws Exception{
+        fillHeap(1000);
+        Thread.sleep(10000);
+    }
+}
