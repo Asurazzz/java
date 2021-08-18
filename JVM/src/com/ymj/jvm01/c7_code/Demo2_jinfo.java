88b281b918ee11c3d04eb373857d27cb40bdfadd
ymj <2513667783@qq.com>
2021-08-18 10:26:24 +0800

    jinfo

--- JVM/src/com/ymj/jvm01/c7_code/Demo2_jinfo.java
+++ JVM/src/com/ymj/jvm01/c7_code/Demo2_jinfo.java
@@ -4,6 +4,16 @@ package com.ymj.jvm01.c7_code;
  * @author : yemingjie
  * @date : 2021/8/17 22:37
  */
+
+/**
+ * jinfo [option] <pid> options参数解释：
+ * - no options 输出所有的系统属性和参数
+ * - -flag <name> 打印指定名称的参数
+ * - -flag [+|-]<name> 打开或关闭参数
+ * - -flag <name>=<value> 设置参数
+ * - -flags 打印所有参数
+ * - -sysprops 打印系统配置
+ */
 public class Demo2_jinfo {
     public static void main(String[] args) {
         System.out.println("jinfo 指令");
