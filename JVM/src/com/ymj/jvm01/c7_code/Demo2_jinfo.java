7f72e56c5cfda948a66a8d8397bab45e5bd041d7
develop3 <2513667783@qq.com>
2021-08-21 09:16:22 +0800

    Demo2_jinfo

--- JVM/src/com/ymj/jvm01/c7_code/Demo2_jinfo.java
+++ JVM/src/com/ymj/jvm01/c7_code/Demo2_jinfo.java
@@ -18,6 +18,7 @@ public class Demo2_jinfo {
     public static void main(String[] args) {
         System.out.println("jinfo 指令");
 
+
         try {
             Thread.sleep(2000000);
         } catch (InterruptedException e) {
