953046b36f44892854a79ca95a6198517081baf4
develop3 <2513667783@qq.com>
2021-11-05 09:34:21 +0800

    模拟频繁young gc 输出gc.log

--- JVM/src/main/java/com/ymj/jvm02/oom/TestHeap.java
+++ JVM/src/main/java/com/ymj/jvm02/oom/TestHeap.java
@@ -1,18 +1,18 @@
-package com.ymj.jvm02.oom;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * -Xms10m -Xmx10m
- */
-public class TestHeap {
-    public static void main(String[] args) {
-        long counter = 0;
-        List<Object> list = new ArrayList<>();
-        while (true) {
-            list.add(new Object());
-            System.out.println("当前创建了第" + (counter) + "个对象");
-        }
-    }
-}
+//package com.ymj.jvm02.oom;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * -Xms10m -Xmx10m
+// */
+//public class TestHeap {
+//    public static void main(String[] args) {
+//        long counter = 0;
+//        List<Object> list = new ArrayList<>();
+//        while (true) {
+//            list.add(new Object());
+//            System.out.println("当前创建了第" + (counter) + "个对象");
+//        }
+//    }
+//}
--- JVM/src/main/java/com/ymj/jvm02/oom/TestMetaspace.java
+++ JVM/src/main/java/com/ymj/jvm02/oom/TestMetaspace.java
@@ -1,37 +1,37 @@
-package com.ymj.jvm02.oom;
-
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
-
-
-/**
- * -XX:MetaspaceSize=10m -XX:MaxMetaspaceSize=10m
- */
-public class TestMetaspace {
-    public static void main(String[] args) {
-        while (true) {
-            Enhancer enhancer = new Enhancer();
-            enhancer.setSuperclass(Car.class);
-            enhancer.setUseCache(false);
-            enhancer.setCallback((MethodInterceptor) (o, method, objects, methodProxy) -> {
-                if ("run".equals(method.getName())) {
-                    System.out.println("启动汽车之前，先进行检查...");
-                    return method.invoke(o, objects);
-                } else {
-                    return method.invoke(o, objects);
-                }
-            });
-
-            Car car = (Car) enhancer.create();
-            car.run();
-        }
-
-    }
-
-
-    static class Car {
-        public void run() {
-            System.out.println("汽车启动，开始行驶....");
-        }
-    }
-}
+//package com.ymj.jvm02.oom;
+//
+//import net.sf.cglib.proxy.Enhancer;
+//import net.sf.cglib.proxy.MethodInterceptor;
+//
+//
+///**
+// * -XX:MetaspaceSize=10m -XX:MaxMetaspaceSize=10m
+// */
+//public class TestMetaspace {
+//    public static void main(String[] args) {
+//        while (true) {
+//            Enhancer enhancer = new Enhancer();
+//            enhancer.setSuperclass(Car.class);
+//            enhancer.setUseCache(false);
+//            enhancer.setCallback((MethodInterceptor) (o, method, objects, methodProxy) -> {
+//                if ("run".equals(method.getName())) {
+//                    System.out.println("启动汽车之前，先进行检查...");
+//                    return method.invoke(o, objects);
+//                } else {
+//                    return method.invoke(o, objects);
+//                }
+//            });
+//
+//            Car car = (Car) enhancer.create();
+//            car.run();
+//        }
+//
+//    }
+//
+//
+//    static class Car {
+//        public void run() {
+//            System.out.println("汽车启动，开始行驶....");
+//        }
+//    }
+//}
