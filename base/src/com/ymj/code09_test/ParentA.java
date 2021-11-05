14bcaf4986fc49de71f26cd925712610cd9cf540
develop3 <2513667783@qq.com>
2021-11-05 11:54:14 +0800

    test

---
+++ base/src/com/ymj/code09_test/ParentA.java
@@ -0,0 +1,29 @@
+package ymj.code09_test;
+
+public class ParentA {
+    static {
+        System.out.println("1");
+    }
+    {
+        System.out.println("aaaaa");
+    }
+
+    public ParentA() {
+        System.out.println("2");
+    }
+}
+
+class SonB extends ParentA {
+    static {
+        System.out.println("a");
+    }
+
+    public SonB() {
+        System.out.println("b");
+    }
+
+    public static void main(String[] args) {
+        ParentA ab = new ParentA(); // 1 a aaaa 2
+        ab = new SonB(); // 1 a aaaa 2 aaaa 2 b
+    }
+}
