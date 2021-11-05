faf359f5bd9d6ed5c53b449b39028711719a0281
develop3 <2513667783@qq.com>
2021-11-05 09:35:05 +0800

    HelloB

---
+++ base/src/com/ymj/code09_test/HelloB.java
@@ -0,0 +1,31 @@
+package ymj.code09_test;
+
+public class HelloB extends HelloA{
+    public HelloB() {
+        System.out.println("B的构造方法");
+    }
+    {
+        System.out.println("B的构造代码块");
+    }
+    static {
+        System.out.println("B的静态代码块");
+    }
+
+    public static HelloB hB = new HelloB();
+
+    public static void main(String[] args) {
+        new HelloB();
+    }
+}
+
+class HelloA {
+    public HelloA() {
+        System.out.println("A的构造方法");
+    }
+    {
+        System.out.println("A的构造代码块");
+    }
+    static {
+        System.out.println("A的静态代码块");
+    }
+}
