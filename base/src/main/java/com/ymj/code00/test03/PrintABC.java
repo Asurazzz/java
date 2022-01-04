2c37ce452c4e6aefaa0f7aeee8b687d73fd03100
develop3 <2513667783@qq.com>
2022-01-04 16:40:39 +0800

    循环打印ABC

---
+++ base/src/main/java/com/ymj/code00/test03/PrintABC.java
@@ -0,0 +1,101 @@
+package com.ymj.code00.test03;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author admin
+ * @version 1.0
+ * @date 2022/1/4 16:00
+ */
+public class PrintABC {
+    ReentrantLock lock = new ReentrantLock();
+    Condition conditionA = lock.newCondition();
+    Condition conditionB = lock.newCondition();
+    Condition conditionC = lock.newCondition();
+    volatile int value = 0;
+    // 打印多少遍
+    private int count;
+
+    public PrintABC(int count) {
+        this.count = count;
+    }
+
+    class ThreadA implements Runnable {
+
+        @Override
+        public void run() {
+            lock.lock();
+            try {
+                for (int i = 0; i < count; i++) {
+                    while (value % 3 != 0) {
+                        conditionA.await();
+                    }
+                    System.out.print("A");
+                    conditionB.signal();
+                    value++;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    class ThreadB implements Runnable {
+
+        @Override
+        public void run() {
+            lock.lock();
+            try {
+                for (int i = 0; i < count; i++) {
+                    while (value % 3 != 1) {
+                        conditionB.await();
+                    }
+                    System.out.print("B");
+                    conditionC.signal();
+                    value++;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    class ThreadC implements Runnable {
+
+        @Override
+        public void run() {
+            lock.lock();
+            try {
+                for (int i = 0; i < count; i++) {
+                    while (value % 3 != 2) {
+                        conditionC.await();
+                    }
+                    System.out.print("C");
+                    conditionA.signal();
+                    value++;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+
+    public void printABC() {
+        new Thread(new ThreadA()).start();
+        new Thread(new ThreadB()).start();
+        new Thread(new ThreadC()).start();
+    }
+
+    public static void main(String[] args) {
+        PrintABC printABC = new PrintABC(15);
+        printABC.printABC();
+    }
+}
