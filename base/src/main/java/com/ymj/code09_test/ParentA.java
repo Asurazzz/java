0d3c038cf87579bbc938b3870d6d63d9b2eeabda
Asurazzz <2513667783@qq.com>
2022-02-07 16:32:36 +0800

    test

--- base/src/main/java/com/ymj/code09_test/ParentA.java
+++ base/src/main/java/com/ymj/code09_test/ParentA.java
@@ -25,7 +25,6 @@ class SonB extends ParentA {
 
     public static void main(String[] args) {
         // 1 a aaaa 2
-
         ParentA ab = new ParentA();
         ab = new SonB(); // 1 a aaaa 2 aaaa 2 b
     }
