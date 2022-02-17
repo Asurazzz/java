896f94474ace0b1d4de8e97f9c5380169499c658
Asurazzz <2513667783@qq.com>
2022-02-17 13:06:49 +0800

    回文数

--- algorithm/src/com/test/Code01_Huiwen.java
+++ algorithm/src/com/test/Code01_Huiwen.java
@@ -5,7 +5,7 @@ package com.test;
  * 文数的所有数字中，大于十进制数 10 的最
  * 小值。
  */
-public class Huiwen {
+public class Code01_Huiwen {
     // 思路：因为是二进制的回文数，如果最低位是0，那么最高位也是0，但是以0开始肯定是不恰当的
     // 由此可知最低位是1。二进制表示的最低位是1，那么就是奇数，所以只用考虑奇数即可
     public static void main(String[] args) {
