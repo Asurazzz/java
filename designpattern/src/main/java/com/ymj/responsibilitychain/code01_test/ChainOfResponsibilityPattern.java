b5dff0a853695a1b7beca307127e483bbbee44cb
Asurazzz <2513667783@qq.com>
2022-02-20 16:55:24 +0800

    添加注释

--- designpattern/src/main/java/com/ymj/responsibilitychain/code01_test/ChainOfResponsibilityPattern.java
+++ designpattern/src/main/java/com/ymj/responsibilitychain/code01_test/ChainOfResponsibilityPattern.java
@@ -1,7 +1,9 @@
 package com.ymj.responsibilitychain.code01_test;
 
 /**
- * 具体的处理逻辑
+ * 责任链（Chain of Responsibility）模式的定义：为了避免请求发送者与多个请求处理者耦合在一起，
+ * 于是将所有请求的处理者通过前一对象记住其下一个对象的引用而连成一条链；
+ * 当有请求发生时，可将请求沿着这条链传递，直到有对象处理它为止。
  */
 public class ChainOfResponsibilityPattern {
     public static void main(String[] args) {
