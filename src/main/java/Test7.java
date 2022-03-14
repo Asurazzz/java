4391bd70254cfcc636769aefc7be811b4177c9dc
yemingjie <2513667783@qq.com>
2022-03-14 19:00:09 +0800

    Test

---
+++ src/main/java/Test7.java
@@ -0,0 +1,11 @@
+/**
+ * @author yemingjie.
+ * @date 2022/3/14.
+ * @time 14:20.
+ */
+public class Test7 {
+    public static void main(String[] args) {
+        String a = "-1";
+        System.out.println(Integer.valueOf(a));
+    }
+}
